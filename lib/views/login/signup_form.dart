f2fe1963e4a042a2a834484232284af7e5069c36
Estelle Choi <estele.choi@gmail.com>
2021-05-25 19:48:05 +0900

    * [NEW] Loading Spinner widget made

--- lib/views/login/signup_form.dart
+++ lib/views/login/signup_form.dart
@@ -10,6 +10,7 @@ import '../../widgets/styles/colors.dart' as colors;
 import '../../widgets/styles/textstyles.dart' as textstyles;
 import '../../widgets/styles/fonts.dart' as fonts;
 import '../../widgets/styles/sizes.dart' as sizes;
+import '../../widgets/spinners/hinoki_spinner.dart';
 
 class SignupForm extends StatefulWidget {
   final bool isSignin;
@@ -23,17 +24,29 @@ class SignupForm extends StatefulWidget {
 
 class _SignupFormState extends State<SignupForm> {
   Map<String, String> formData = {'id': '', 'password': '', 'type': 'email'};
+  bool _isLoading = false;
 
   void login(BuildContext context) async {
-    print('login data');
-    print(formData.toString());
+    setState(() {
+      _isLoading = true;
+    });
+
     try {
       await api.login(formData);
       await appState.getGuideUnreadCnt();
       appState.login();
+
+      setState(() {
+        _isLoading = false;
+      });
+
       widget.onSuccess();
     } catch (e) {
-      // ...
+      setState(() {
+        _isLoading = false;
+      });
+
+      print(e);
     }
   }
 
@@ -53,104 +66,111 @@ class _SignupFormState extends State<SignupForm> {
   Widget build(BuildContext context) {
     String toggleText = widget.isSignin ? 'in' : 'up';
 
-    return AppBarLayout(
-        extendBodyBehindAppBar: true,
-        title: '',
-        scrollController: ScrollController(),
-        // globalKey: GlobalKey(),
-        body: Container(
-            height: sizes.screenHeight(context),
-            padding: EdgeInsets.only(
-              top: sizes.appBar * 2,
-              bottom: 30,
-              left: 26,
-              right: 26,
-            ),
-            decoration: BoxDecoration(
-              color: colors.white,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    // decoration: BoxDecoration(border: borders.primaryDeco),
-                    child: Column(
+    return Stack(
+      children: <Widget>[
+        AppBarLayout(
+            extendBodyBehindAppBar: true,
+            title: '',
+            scrollController: ScrollController(),
+            // globalKey: GlobalKey(),
+            body: Container(
+                height: sizes.screenHeight(context),
+                padding: EdgeInsets.only(
+                  top: sizes.appBar * 2,
+                  bottom: 30,
+                  left: 26,
+                  right: 26,
+                ),
+                decoration: BoxDecoration(
+                  color: colors.white,
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
                     Container(
-                        margin: EdgeInsets.symmetric(vertical: 40),
-                        child: Text('Sign $toggleText with email.',
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                                color: colors.black,
-                                fontSize: 20,
-                                fontFamily: fonts.primary,
-                                fontFamilyFallback: fonts.primaryFallbacks))),
-                    ShadowedInputBundle(
+                        // decoration: BoxDecoration(border: borders.primaryDeco),
+                        child: Column(
                       children: <Widget>[
-                        LinkedInput(
-                          position: 'top',
-                          labelText: 'Email',
-                          defaultValue: '',
-                          onChanged: _changeId,
+                        Container(
+                            margin: EdgeInsets.symmetric(vertical: 40),
+                            child: Text('Sign $toggleText with email.',
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: colors.black,
+                                    fontSize: 20,
+                                    fontFamily: fonts.primary,
+                                    fontFamilyFallback:
+                                        fonts.primaryFallbacks))),
+                        ShadowedInputBundle(
+                          children: <Widget>[
+                            LinkedInput(
+                              position: 'top',
+                              labelText: 'Email',
+                              defaultValue: '',
+                              onChanged: _changeId,
+                            ),
+                            LinkedInput(
+                              type: 'password',
+                              position: 'bottom',
+                              labelText: 'Password',
+                              defaultValue: '',
+                              onChanged: _changePassword,
+                            ),
+                          ],
                         ),
-                        LinkedInput(
-                          type: 'password',
-                          position: 'bottom',
-                          labelText: 'Password',
-                          defaultValue: '',
-                          onChanged: _changePassword,
-                        ),
+                        Container(
+                            margin: EdgeInsets.only(top: 20),
+                            child: HinokiButton(
+                              fullWidth: true,
+                              type: 'filled',
+                              color: 'primary',
+                              label: widget.isSignin
+                                  ? 'Sign in'
+                                  : 'Create an account',
+                              onPressed: () {
+                                login(context);
+                              },
+                            )),
                       ],
-                    ),
+                    )),
                     Container(
-                        margin: EdgeInsets.only(top: 20),
-                        child: HinokiButton(
-                          fullWidth: true,
-                          type: 'filled',
-                          color: 'primary',
-                          label:
-                              widget.isSignin ? 'Sign in' : 'Create an account',
-                          onPressed: () {
-                            login(context);
-                          },
-                        )),
+                        // margin: EdgeInsets.only(top: 30, bottom: 0),
+                        child: RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                                style: TextStyle(color: colors.disabled),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                      text: 'By Signing up, you agree to our ',
+                                      style: textstyles.fadedComment),
+                                  TextSpan(
+                                    text: 'Terms of Service',
+                                    style: textstyles.comment,
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        // Open Browser to see ..
+                                      },
+                                  ),
+                                  TextSpan(
+                                      text: ' and acknowledge that our ',
+                                      style: textstyles.fadedComment),
+                                  TextSpan(
+                                    text: 'Privacy Policy',
+                                    style: textstyles.comment,
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        // Open Browser to see ..
+                                      },
+                                  ),
+                                  TextSpan(
+                                      text: ' applies to you.',
+                                      style: textstyles.fadedComment),
+                                ])))
                   ],
-                )),
-                Container(
-                    // margin: EdgeInsets.only(top: 30, bottom: 0),
-                    child: RichText(
-                        textAlign: TextAlign.center,
-                        text: TextSpan(
-                            style: TextStyle(color: colors.disabled),
-                            children: <TextSpan>[
-                              TextSpan(
-                                  text: 'By Signing up, you agree to our ',
-                                  style: textstyles.fadedComment),
-                              TextSpan(
-                                text: 'Terms of Service',
-                                style: textstyles.comment,
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    // Open Browser to see ..
-                                  },
-                              ),
-                              TextSpan(
-                                  text: ' and acknowledge that our ',
-                                  style: textstyles.fadedComment),
-                              TextSpan(
-                                text: 'Privacy Policy',
-                                style: textstyles.comment,
-                                recognizer: TapGestureRecognizer()
-                                  ..onTap = () {
-                                    // Open Browser to see ..
-                                  },
-                              ),
-                              TextSpan(
-                                  text: ' applies to you.',
-                                  style: textstyles.fadedComment),
-                            ])))
-              ],
-            )));
+                ))),
+        _isLoading ? HinokiSpinner(color: colors.primary) : Container()
+      ],
+    );
   }
 }
---
+++ lib/widgets/spinners/hinoki_spinner.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import '../../widgets/styles/colors.dart' as colors;
+import '../../animations/delay_tween.dart';
+
+class HinokiSpinner extends StatefulWidget {
+  final Color color;
+  final String size;
+
+  HinokiSpinner({this.color = colors.primary, this.size = "normal"});
+
+  @override
+  _HinokiSpinnerState createState() => _HinokiSpinnerState();
+}
+
+class _HinokiSpinnerState extends State<HinokiSpinner>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 400));
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(color: colors.whiteAlphaDeep),
+        ),
+        Center(
+          child: SizedBox.fromSize(
+              size: Size(100, 50),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: List.generate(3, (i) {
+                    return ScaleTransition(
+                        scale: DelayTween(begin: 0, end: 1, delay: i * 0.2)
+                            .animate(_animationController),
+                        child: SizedBox.fromSize(
+                            size: Size.square(25),
+                            child: DecoratedBox(
+                                decoration: BoxDecoration(
+                                    color: widget.color,
+                                    shape: BoxShape.circle))));
+                  }))),
+        )
+      ],
+    );
+  }
+}
